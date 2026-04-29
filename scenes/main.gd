cd72a6fc33e1ae811acb09f99bed753262183d0e
Meriam <meriamalubaidi@gmail.com>
2026-04-29 20:32:21 +0100

    Add volume and pitch controls for drum pads

--- scenes/main.gd
+++ scenes/main.gd
@@ -10,11 +10,20 @@ extends Control
 @onready var hihat_button = $PadGrid/TheHiHatPad
 @onready var clap_button = $PadGrid/TheClapButton
 
+@onready var volume_slider = $ControlsBox/VolumeSlider
+@onready var pitch_slider = $ControlsBox/PitchSlider
+
 func _ready():
 	kick_button.pressed.connect(_on_kick_pressed)
 	snare_button.pressed.connect(_on_snare_pressed)
 	hihat_button.pressed.connect(_on_hihat_pressed)
 	clap_button.pressed.connect(_on_clap_pressed)
+
+	volume_slider.value_changed.connect(_on_volume_changed)
+	pitch_slider.value_changed.connect(_on_pitch_changed)
+
+	_on_volume_changed(volume_slider.value)
+	_on_pitch_changed(pitch_slider.value)
 
 func flash_button(button: Button):
 	var original_modulate = button.modulate
@@ -37,6 +46,18 @@ func _on_hihat_pressed():
 func _on_clap_pressed():
 	clap_audio.play()
 	flash_button(clap_button)
+
+func _on_volume_changed(value):
+	kick_audio.volume_db = value
+	snare_audio.volume_db = value
+	hihat_audio.volume_db = value
+	clap_audio.volume_db = value
+
+func _on_pitch_changed(value):
+	kick_audio.pitch_scale = value
+	snare_audio.pitch_scale = value
+	hihat_audio.pitch_scale = value
+	clap_audio.pitch_scale = value
 
 func _input(event):
 	if event.is_action_pressed("kick"):
